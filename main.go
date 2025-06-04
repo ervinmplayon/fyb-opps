657ebd3bd61458f3a17a6381098128b80aaa0642
ervz <ervin.mitra@playonsports.com>
2025-06-03 20:28:52 -0400

    handle api

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"time"
 
@@ -11,6 +12,15 @@ import (
 
 var defaultLimiter = ratelimiter.NewLimiter(2 * time.Second)
 var hardThrottleLimiter = ratelimiter.NewHardThrottleLimiter(5, time.Second)
+var ipLimiter = ratelimiter.NewLimiterMap(5, time.Second)
+
+func getIP(r *http.Request) string {
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return r.RemoteAddr // * fallback
+	}
+	return ip
+}
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
@@ -31,6 +41,16 @@ func hardThrottleHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "OK - You passed the vibe check")
 }
 
+func ipLimiterHandler(w http.ResponseWriter, r *http.Request) {
+	ip := getIP(r)
+	if !ipLimiter.Allow(ip) {
+		http.Error(w, "Rate Limit exceeded", http.StatusTooManyRequests)
+		return
+	}
+
+	fmt.Fprintf(w, "Msg from %s. You're within the rate limit.\n", ip)
+}
+
 func main() {
 	defer defaultLimiter.Stop() // * Clean up the ticker
 
@@ -44,6 +64,7 @@ func main() {
 	http.HandleFunc("/", defaultHandler)
 	http.HandleFunc("/not-a-honeypot", hardThrottleHandler)
 	http.HandleFunc("/api", hardThrottleHandler)
+	http.HandleFunc("/exclusive-content", ipLimiterHandler)
 	log.Println("Serving on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
