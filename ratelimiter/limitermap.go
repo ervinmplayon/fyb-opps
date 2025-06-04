de76270c261a0f50475236d217adea748ea38844
ervz <ervin.mitra@playonsports.com>
2025-06-03 20:12:53 -0400

    initialize the struct

--- ratelimiter/limitermap.go
+++ ratelimiter/limitermap.go
@@ -1,3 +1,15 @@
 package ratelimiter
 
-// *
+import (
+	"sync"
+	"time"
+)
+
+// * IP-based limiter manager, add a wrapper to manager rate limiters per IP.
+
+type LimiterMap struct {
+	limiters map[string]*HardThrottleLimiter
+	mu       sync.Mutex
+	limit    int
+	interval time.Duration
+}
